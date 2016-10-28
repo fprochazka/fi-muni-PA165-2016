4ea4c83458608cb3c305025dc09ed58d4cc204ee
Filip Procházka <filip@prochazka.su>
2016-10-28 20:08:41 +0200

    task01

--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
@@ -1,35 +1,43 @@
 package cz.fi.muni.pa165.tasks;
 
+import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
+import cz.fi.muni.pa165.entity.Category;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
-import cz.fi.muni.pa165.entity.Category;
+import static org.testng.Assert.*;
 
 
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class Task01 extends AbstractTransactionalTestNGSpringContextTests
+{
 
-public class Task01  {
+    @PersistenceUnit
+    private EntityManagerFactory emf;
 
-	
-	@PersistenceUnit
-	private EntityManagerFactory emf;
+    @Test
+    public void categoryTest()
+    {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Category cat = new Category();
+        cat.setName("Test");
+        em.persist(cat);
+        em.getTransaction().commit();
+        em.close();
 
-	@Test
-	public void categoryTest() {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		Category cat = new Category();
-		cat.setName("Test");
-		em.persist(cat);
-		em.getTransaction().commit();
-		em.close();
-		//TODO under this line: create a second entity manager in categoryTest, use find method to find the category and assert its name.
-	}
+        //TODO under this line: create a second entity manager in categoryTest, use find method to find the category and assert its name.
+        EntityManager em2 = emf.createEntityManager();
+        em2.getTransaction().begin();
+        Category foundCategory = em2.find(Category.class, cat.getId());
+        assertEquals("Test", foundCategory.getName());
+        em2.getTransaction().commit();
+        em2.close();
+    }
 
 }
